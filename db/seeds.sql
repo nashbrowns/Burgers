7550797f7c2561364955b0304a4ced5d8a49631d
Nash McDonald <nash42192@gmail.com>
2019-04-26 18:53:09 -0700

    dynamically displaying burgers through handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steak Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cat Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
